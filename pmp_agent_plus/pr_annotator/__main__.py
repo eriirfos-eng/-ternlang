f338f020cff8023d276dc59ead5104a6f7ea2850
rfi-irfos <rfi.irfos@gmail.com>
2025-09-02 12:18:19 +0000

    Update __main__.py
    Signed-off-by: rfi-irfos <rfi.irfos@gmail.com>

--- pmp_agent_plus/pr_annotator/__main__.py
+++ pmp_agent_plus/pr_annotator/__main__.py
@@ -1,3 +1,24 @@
 from .annotate import main
 if __name__ == '__main__':
     main()
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-5"
+token = os.environ["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"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage("You are a helpful assistant."),
+        UserMessage("What is the capital of France?"),
+    ],
+    model=model
+)
