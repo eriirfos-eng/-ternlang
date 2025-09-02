0c5fc3288ea6a6f589ff7fe3ba42cd9e0ecbbfe5
rfi-irfos <rfi.irfos@gmail.com>
2025-09-02 12:15:52 +0000

    Update core.py
    Signed-off-by: rfi-irfos <rfi.irfos@gmail.com>

--- divided-by-zero-agent/divided_by_zero/core.py
+++ divided-by-zero-agent/divided_by_zero/core.py
@@ -1,3 +1,25 @@
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
+
+endpoint = "https://models.github.ai/inference"
+model = "openai/gpt-5"
+token = os.environ["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"]
+
+client = ChatCompletionsClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(token),
+)
+
+response = client.complete(
+    messages=[
+        SystemMessage("You are a helpful assistant."),
+        UserMessage("What is the capital of France?"),
+    ],
+    model=model
+)
+
 from pydantic import BaseModel, Field
 
 REFRAIN = -1
